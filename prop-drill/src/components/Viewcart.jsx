5a01450582ada46d31cb45a84e58588757f71816
AnandhaKumar S <anandhakumarnathan@gmail.com>
2024-05-24 15:02:08 +0530

    shopcart updated

---
+++ prop-drill/src/components/Viewcart.jsx
@@ -0,0 +1,33 @@
+/* eslint-disable react/prop-types */
+import { useEffect, useState, useContext } from "react";
+import "./Viewcart.css";
+import { cartContext } from "./cartContext";
+
+export const Viewcart = () => {
+  const { cart } = useContext(cartContext);
+  const [total, setTotal] = useState(0);
+
+  useEffect(() => {
+    setTotal(cart.reduce((acc, curr) => acc + parseInt(curr.amt), 0));
+  }, [cart]);
+
+  return (
+    <>
+      <h1 className="cart-heading">Cart Products</h1>
+      <div className="cart-container">
+        {cart.map((product) => (
+          <div className="cart-product" key={product.id}>
+            <div className="img">
+              <img src={product.pic} alt="image" />
+            </div>
+            <div className="cart-product-details">
+              <h3>{product.name}</h3>
+              <p>Price Rs: {product.amt}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+      <h2 className="cart-amount">Total amount Rs: {total}</h2>
+    </>
+  );
+};
